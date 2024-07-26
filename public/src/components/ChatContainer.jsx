9acb27ec2320166a64e60379aa18020a87b77aaf
AKASH PAL <akashpal2122003@gmail.com>
2024-07-27 02:01:03 +0530

    half components

---
+++ public/src/components/ChatContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function ChatContainer({ currentUser }) {
+    return (
+        <Container>
+            <div className="chat-header">
+                <div className="user-datils">
+                    <div className="avatar"></div>
+                    <div className="username">
+                        <h3>{currentUser.username}</h3>
+                    </div>
+                </div>
+            </div>
+        </Container>
+    );
+} 
+
+const Container = styled.div``;
--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,3 +1,4 @@
+const { execArgv } = require("process");
 const User = require("../model/userModel");
 const bcrypt = require("bcrypt");
 
@@ -30,6 +31,37 @@ module.exports.login = async (req, res, next) => {
         if (!isPasswordValid) return res.json({ msg: "Incorrect username or password", status: false });
         delete user.password;
         return res.json({ status: true, user });
+    } catch (ex) {
+        next(ex);
+    }
+};
+
+module.exports.setAvatar = async (req, res, next) => {
+    try {
+        const userId = req.params.id;
+        const avatarImage = req.body.image;
+        const userData = await User.findByIdAndUpdate(userId, {
+            isAvatarImageSet: true,
+            avatarImage,
+        });
+        return res.json({
+            isSet: userData.isAvatarImageSet,
+            image: userData.avatarImage,
+        })
+    } catch (ex) {
+        next(ex);
+    }
+};
+
+module.exports.getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find({ _id: { $ne: req.params.id } }).select([
+            "email",
+            "username",
+            "avatarImage",
+            "_id",
+        ]);
+        return res.json(users);
     } catch (ex) {
         next(ex);
     }
