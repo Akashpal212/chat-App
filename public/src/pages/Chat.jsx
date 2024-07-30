15852d68678eabd950c2443b1841cdd997a449cf
AKASH PAL <akashpal2122003@gmail.com>
2024-07-30 18:04:27 +0530

    socketio

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
-import { allUsersRoute } from '../utils/APIRoutes'
+import { allUsersRoute, host } from '../utils/APIRoutes'
 import Contacts from "../components/Contacts"
 import Welcome from "../components/Welcome"
 import ChatContainer from '../components/ChatContainer'
+import { io } from 'socket.io-client'
 
 function Chat() {
+  const socket = useRef();
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
   const navigate = useNavigate();
@@ -28,6 +30,12 @@ function Chat() {
     })
     ();
   }, []);
+  useEffect(() => {
+    if (currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);
+    }
+  }, [currentUser]);
   useEffect(() => {
     (async () => {
       try {
@@ -53,7 +61,7 @@ function Chat() {
     <Container>
       <div className="container">
         <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
-        {isLoaded && currentChat === undefined ? (<Welcome currentUser={currentUser} />) : (<ChatContainer currentChat={currentChat} />)}
+        {isLoaded && currentChat === undefined ? (<Welcome currentUser={currentUser} />) : (<ChatContainer currentChat={currentChat} currentUser={currentUser} socket={socket} />)}
       </div>
     </Container>
   );
